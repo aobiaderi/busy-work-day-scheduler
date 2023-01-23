d35907096d207461b3f1c3ddb1f7906b3c80c4d7
Obee123 <aobiaderi@yahoo.com>
2023-01-23 22:39:32 +0000

    added if statement for class attribute and color coding to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,45 @@ $("#4Row").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
 $("#5Row").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
 
 
+$(document).ready(function () {
+
+    // Function to store user input 
+    showUserInput();
+    
+    // Show Date and Time in jumbotron section 
+    
+    function addDate() { 
+    $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm a'));
+    
+    // Update the time every second or minute
+    } setInterval(addDate, 1000);
+    
+        addDate();
+    
+    // Loop through to add attributes for each input to reflect the current hour and Change the corresponding color in each row 
+    for (var i = 0; i <= 12; i++) {  
+    
+    // Variable for the hour of the row
+        var inputHour = $("#" + i + "Row").attr("data-time");  
+    
+        // Parses the hour so it returns as an integer
+        var inputHourInt = parseInt(inputHour); 
+        
+    // If statement to add a class attribute and apply red color if time is within the present hour
+        if (currentHourInt === inputHourInt) {
+            $("#" + i + "Row").addClass("present"); 
+        }
+    
+    // If statement to add a class attribute and apply grey color if time is is in the past
+        if (currentHourInt > inputHourInt) {  
+            $("#" + i + "Row").addClass("past");
+        }
+    // If statement to add a class attribute and apply green color if time is is in the future
+    
+        if (currentHourInt < inputHourInt) { 
+            $("#" + i + "Row").addClass("future");
+        }
+    }
+    
+    
+    });
